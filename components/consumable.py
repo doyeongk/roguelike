0044d56c031f79c71fee397abb74b6bf40ee3356
doyeongk <82367604+doyeongk@users.noreply.github.com>
2023-06-26 15:35:29 +1000

    feat: added potions and inventory

--- components/consumable.py
+++ components/consumable.py
@@ -4,6 +4,7 @@ from typing import Optional, TYPE_CHECKING
 
 import actions
 import colour
+import components.inventory
 from components.base_component import BaseComponent
 from exceptions import Impossible
 
@@ -24,7 +25,13 @@ class Consumable(BaseComponent):
         `action` is the context for this activation.
         """
         raise NotImplementedError()
-
+    
+    def consume(self) -> None:
+        """Remove the consumed item from its containing inventory."""
+        entity = self.parent
+        inventory = entity.parent
+        if isinstance(inventory, components.inventory.Inventory):
+            inventory.items.remove(entity)
 
 class HealingConsumable(Consumable):
     def __init__(self, amount: int):
@@ -39,5 +46,6 @@ class HealingConsumable(Consumable):
                 f"You consume the {self.parent.name}, and recover {amount_recovered} HP!",
                 colour.health_recovered,
             )
+            self.consume()
         else:
             raise Impossible(f"Your health is already full.")
